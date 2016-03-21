77476bfcd9b397bf14cd4ae7e09156a6c03c1f0f
Beto Barros <betobarros07@gmail.com>
2016-03-21 10:52:52 -0300

    Rename the project name to EasyImage.

---
+++ src/easyImage/EasyImage.Net45/ImageExtensions.cs
@@ -0,0 +1,98 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace ImageCrop.Net45
+{
+    /// <summary>
+    /// Image extensions class for .Net Framework 4.5.
+    /// <para/>Here you have all the resize and crop functions for Image class, Have fun!.
+    /// </summary>
+    public static class ImageExtensions
+    {
+        /// <summary>
+        /// Resize the image based on height.
+        /// <para/>Keeps width proportional.
+        /// </summary>
+        /// <example>
+        /// How to use HeightResize function:
+        /// <code>
+        /// var image = Image.FromFile("C:\\images\\bar.jpg");
+        /// image.HeightResize(500);
+        /// </code>
+        /// </example>
+        /// <see cref="Image.FromFile(string)"/>
+        /// <seealso cref="Resize(Image, int, int)"/>
+        /// <param name="image">The image to be resized.</param>
+        /// <param name="height">The new height of image.</param>
+        /// <returns>The new resized image.</returns>
+        public static Bitmap HeightResize(this Image image, int height)
+        {
+            var width = (height * image.Width) / image.Height;
+            return image.Resize(width, height);
+        }
+
+        /// <summary>
+        /// Resize the image.
+        /// </summary>
+        /// <example>
+        /// How to use Resize function:
+        /// <code>
+        /// var image = Image.FromFile("C:\\images\\foo-bar.jpg");
+        /// image.HeightResize(400, 500);
+        /// </code>
+        /// </example>
+        /// <see cref="Image.FromFile(string)"/>
+        /// <param name="image">The image to be resized.</param>
+        /// <param name="width">The new width of image.</param>
+        /// <param name="height">The new height of image.</param>
+        /// <returns>The new resized image.</returns>
+        public static Bitmap Resize(this Image image, int width, int height)
+        {
+            var bitmap = new Bitmap(width, height);
+            var horizontalResolution = image.HorizontalResolution;
+            var verticalResolution = image.VerticalResolution;
+            bitmap.SetResolution(horizontalResolution, verticalResolution);
+            using (var graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+
+                using (var imageAttributes = new ImageAttributes())
+                {
+                    var widthStart = 0;
+                    var heightStart = 0;
+                    imageAttributes.SetWrapMode(WrapMode.TileFlipXY);
+                    var rectangle = new Rectangle(widthStart, heightStart, width, height);
+                    graphics.DrawImage(image, rectangle, widthStart, heightStart, image.Width, image.Height, GraphicsUnit.Pixel, imageAttributes);
+                }
+            }
+            return bitmap;
+        }
+
+        /// <summary>
+        /// Resize the image based on width.
+        /// <para/>Keeps height proportional.
+        /// </summary>
+        /// <example>
+        /// How to use WidthResize function:
+        /// <code>
+        /// var image = Image.FromFile("C:\\images\\foo.jpg");
+        /// image.WidthResize(400);
+        /// </code>
+        /// </example>
+        /// <see cref="Image.FromFile(string)"/>
+        /// <seealso cref="Resize(Image, int, int)"/>
+        /// <param name="image">The image to be resized.</param>
+        /// <param name="width">The new width of image.</param>
+        /// <returns>The new resized image.</returns>
+        public static Bitmap WidthResize(this Image image, int width)
+        {
+            var height = (width * image.Height) / image.Width;
+            return image.Resize(width, height);
+        }
+    }
+}
---
+++ src/easyImage/EasyImage.Tests/Net45Test.cs
@@ -0,0 +1,89 @@
+﻿using FluentAssertions;
+using System;
+using System.Drawing;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Reflection;
+
+namespace ImageCrop.Tests
+{
+    using Net45;
+
+    /// <summary>
+    /// Test class for ImageExtensions used in .Net Framework 4.5
+    /// </summary>
+    [TestClass]
+    public class Net45Test
+    {
+        /// <summary>
+        /// Absolute path to get the images.
+        /// </summary>
+        private readonly string _absolutePath;
+
+        /// <summary>
+        /// Initialize _absolutePath.
+        /// </summary>
+        public Net45Test()
+        {
+            _absolutePath= Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location).Replace("bin\\Debug", "");
+        }
+
+        /// <summary>
+        /// Get a image from Images directory.
+        /// </summary>
+        /// <param name="imageName">Name of image from Images directory.</param>
+        /// <returns>Image from Images directory</returns>
+        private Image GetImage(string imageName)
+        {
+            var path = Path.Combine(_absolutePath, "Images\\" + imageName);
+            return Image.FromFile(path);
+        }
+
+        private string Save(Image image)
+        {
+            var fileName = Guid.NewGuid().ToString() + ".jpg";
+            var pathTo = Path.Combine(_absolutePath, "Images\\Results\\", fileName);
+            image.Save(pathTo);
+            return pathTo;
+        }
+
+        [TestMethod]
+        public void DoResize()
+        {
+            var image = GetImage("tulips.jpg");
+            var resize = image.Resize(800, 450);
+            var path = Save(resize);
+            var resizedImage = Image.FromFile(path);
+            resizedImage.Width.Should().Be(800, "Is the width for the resized image.");
+            resizedImage.Height.Should().Be(450, "Is the height for the resized image.");
+        }
+
+        [TestMethod]
+        public void DoWidthResize()
+        {
+            var image = GetImage("koala.jpg");
+            var resize = image.WidthResize(665);
+            var path = Save(resize);
+            var resizedImage = Image.FromFile(path);
+            // New heigth is:
+            // (768 * 665) / 1024 = 498
+            resizedImage.Width.Should().Be(665, "Is the width for the resized image.");
+            resizedImage.Height.Should().Be(498, "Is the height for the resized image.");
+        }
+
+        [TestMethod]
+        public void DoHeightResize()
+        {
+            var image = GetImage("desert.jpg");
+            var resize = image.HeightResize(499);
+            var path = Save(resize);
+            var resizedImage = Image.FromFile(path);
+            // New heigth is:
+            // (1024 * 499) / 768 = 665
+            resizedImage.Width.Should().Be(665, "Is the width for the resized image.");
+            resizedImage.Height.Should().Be(499, "Is the height for the resized image.");
+        }
+    }
+}
