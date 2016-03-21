88b6d614f424105f82f0cac086999e0a15c5cf40
Beto Barros <betobarros07@gmail.com>
2016-03-21 14:17:32 -0300

    Add test method for Crop function.

--- src/easyImage/EasyImage.Tests/Net45Test.cs
+++ src/easyImage/EasyImage.Tests/Net45Test.cs
@@ -1,12 +1,13 @@
 ﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Drawing;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
 using System.Reflection;
 
 namespace EasyImage.Tests
 {
+    // Reference of Library EasyImage.Net45
     using Net45;
 
     /// <summary>
@@ -53,6 +54,39 @@ namespace EasyImage.Tests
         }
 
         /// <summary>
+        /// Do a simple Crop.
+        /// </summary>
+        [TestMethod]
+        public void DoCrop()
+        {
+            var image = GetImage("hydrangeas.jpg");
+            var crop = image.Crop(120, 50);
+            var path = Save(crop);
+            var croppedImage = Image.FromFile(path);
+            // New size is:
+            // 768 - 50 * 2 = 668 height
+            // 1024 - 120 * 2 = 784 width
+            croppedImage.Width.Should().Be(784, "Is the width for the cropped image.");
+            croppedImage.Height.Should().Be(668, "Is the height for the cropped image.");
+        }
+
+        /// <summary>
+        /// Do a simple Height resize.
+        /// </summary>
+        [TestMethod]
+        public void DoHeightResize()
+        {
+            var image = GetImage("desert.jpg");
+            var resize = image.HeightResize(499);
+            var path = Save(resize);
+            var resizedImage = Image.FromFile(path);
+            // New width is:
+            // (1024 * 499) / 768 = 665
+            resizedImage.Width.Should().Be(665, "Is the width for the resized image.");
+            resizedImage.Height.Should().Be(499, "Is the height for the resized image.");
+        }
+
+        /// <summary>
         /// Do a simple resize.
         /// </summary>
         [TestMethod]
@@ -80,22 +114,6 @@ namespace EasyImage.Tests
             // (768 * 665) / 1024 = 498
             resizedImage.Width.Should().Be(665, "Is the width for the resized image.");
             resizedImage.Height.Should().Be(498, "Is the height for the resized image.");
-        }
-
-        /// <summary>
-        /// Do a simple Height resize.
-        /// </summary>
-        [TestMethod]
-        public void DoHeightResize()
-        {
-            var image = GetImage("desert.jpg");
-            var resize = image.HeightResize(499);
-            var path = Save(resize);
-            var resizedImage = Image.FromFile(path);
-            // New width is:
-            // (1024 * 499) / 768 = 665
-            resizedImage.Width.Should().Be(665, "Is the width for the resized image.");
-            resizedImage.Height.Should().Be(499, "Is the height for the resized image.");
         }
     }
 }
