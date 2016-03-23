32d582502a323d54ecceffbb40ee19d83b45033e
Beto Barros <betobarros07@gmail.com>
2016-03-23 11:11:23 -0300

    Fix VisualStudio namespace.

--- src/easyImage/EasyImage.Tests/Net45Test.cs
+++ src/easyImage/EasyImage.Tests/Net45Test.cs
@@ -1,14 +1,10 @@
 ﻿using FluentAssertions;
-#if NUNIT
 using TestClass = NUnit.Framework.TestFixtureAttribute;
 using TestMethod = NUnit.Framework.TestAttribute;
-using TestCleanup = NUnit.Framework.TearDownAttribute;
-using TestInitialize = NUnit.Framework.SetUpAttribute;
-using ClassCleanup = NUnit.Framework.TestFixtureTearDownAttribute;
-using ClassInitialize = NUnit.Framework.TestFixtureSetUpAttribute;
-#else
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-#endif
+//using TestCleanup = NUnit.Framework.TearDownAttribute;
+//using TestInitialize = NUnit.Framework.SetUpAttribute;
+//using ClassCleanup = NUnit.Framework.TestFixtureTearDownAttribute;
+//using ClassInitialize = NUnit.Framework.TestFixtureSetUpAttribute;
 using System;
 using System.Drawing;
 using System.IO;
