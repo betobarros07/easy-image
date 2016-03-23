8fc7e02742b11acd66422f0e2c820b39042c1e6a
Beto Barros <betobarros07@gmail.com>
2016-03-23 09:01:48 -0300

    Test method for complex crop.

--- src/easyImage/EasyImage.Tests/Net45Test.cs
+++ src/easyImage/EasyImage.Tests/Net45Test.cs
@@ -26,7 +26,7 @@ namespace EasyImage.Tests
         /// </summary>
         public Net45Test()
         {
-            _absolutePath= Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location).Replace("bin\\Debug", "");
+            _absolutePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location).Replace("bin\\Debug", "");
         }
 
         /// <summary>
@@ -54,20 +54,20 @@ namespace EasyImage.Tests
         }
 
         /// <summary>
-        /// Do a simple Crop.
+        /// Do a complex Crop.
         /// </summary>
         [TestMethod]
-        public void DoCrop()
+        public void DoComplexCrop()
         {
-            var image = GetImage("hydrangeas.jpg");
-            var crop = image.Crop(120, 50);
+            var image = GetImage("penguins.jpg");
+            var crop = image.Crop(70, 130, 150, 50);
             var path = Save(crop);
             var croppedImage = Image.FromFile(path);
             // New size is:
-            // 768 - 50 * 2 = 668 height
-            // 1024 - 120 * 2 = 784 width
-            croppedImage.Width.Should().Be(784, "Is the width for the cropped image.");
-            croppedImage.Height.Should().Be(668, "Is the height for the cropped image.");
+            // 1024 - (70 + 30) = 824 width
+            // 768 - (50 + 150) = 568 height
+            croppedImage.Width.Should().Be(824, "Is the width for the cropped image.");
+            croppedImage.Height.Should().Be(568, "Is the height for the cropped image.");
         }
 
         /// <summary>
@@ -98,6 +98,23 @@ namespace EasyImage.Tests
             var resizedImage = Image.FromFile(path);
             resizedImage.Width.Should().Be(800, "Is the width for the resized image.");
             resizedImage.Height.Should().Be(450, "Is the height for the resized image.");
+        }
+
+        /// <summary>
+        /// Do a simple Crop.
+        /// </summary>
+        [TestMethod]
+        public void DoSimpleCrop()
+        {
+            var image = GetImage("hydrangeas.jpg");
+            var crop = image.Crop(120, 50);
+            var path = Save(crop);
+            var croppedImage = Image.FromFile(path);
+            // New size is:
+            // 1024 - 120 * 2 = 784 width
+            // 768 - 50 * 2 = 668 height
+            croppedImage.Width.Should().Be(784, "Is the width for the cropped image.");
+            croppedImage.Height.Should().Be(668, "Is the height for the cropped image.");
         }
 
         /// <summary>
